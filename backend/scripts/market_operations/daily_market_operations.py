2177b7718861c8b6c698e1cf278c54bce54b1710
ori.f <orifreedman@gmail.com>
2020-07-02 17:48:50 +0300

    added a little bit, but some was redundant

--- backend/scripts/market_operations/daily_market_operations.py
+++ backend/scripts/market_operations/daily_market_operations.py
@@ -3,8 +3,9 @@ import backend.scripts.analysis.stocks_analysis as stocks_analysis
 import backend.scripts.analysis.stocks_analysis_API as stocks_API
 import backend.scripts.data_scrape.path_finding_functions as path_finding_functions
 import backend.scripts.analysis.advanced_utils as advanced_utils
+import statistics
 
-COMPANIES_BLACK_LIST = ['Unnamed: 0', 'TRNX']
+COMPANIES_BLACK_LIST = ['TRNX']
 
 
 def remove_companies_black_list_from_dataframe(dataframe):
@@ -18,12 +19,16 @@ def get_filtered_selected_points_dataframe(exchange_name, ascend_or_descend, sig
     points_dataframe_file_path = path_finding_functions. \
         get_points_file_path(exchange_name, ascend_or_descend, sign_or_value, delay_days)
     points_dataframe = pd.read_excel(points_dataframe_file_path)
-    filtered_points_dataframe = points_dataframe.drop(columns=COMPANIES_BLACK_LIST)
+    points_dataframe = points_dataframe.set_index('Unnamed: 0')
+
+    filtered_points_dataframe = points_dataframe.drop(columns=COMPANIES_BLACK_LIST,
+                                                      index=COMPANIES_BLACK_LIST,
+                                                      errors='ignore')
     return filtered_points_dataframe
 
 
 
-def top_stocks_today(exchange_name, delay_days):
+def top_stocks_today(exchange_name, delay_days, sign_percent_filter=0.8, minimum_eligible_companies=3):
     exchange_dataframe_today = stocks_API.AllDataAnalysisToday(exchange_name).all_daily_dataframe
     exchange_dataframe_today_filtered = remove_companies_black_list_from_dataframe(exchange_dataframe_today)
 
@@ -31,5 +36,28 @@ def top_stocks_today(exchange_name, delay_days):
         get_filtered_selected_points_dataframe(exchange_name, 'ascend', 'sign', delay_days)
 
     all_symbols_list = list(filtered_ascend_sign_points_dataframe)
-    print(all_symbols_list[:10])
-    print(len(all_symbols_list))
+
+    # thats not what im supposed to do. i need to gather the points according to
+    # the values in exchange_dataframe_today_filtered
+    """top_points_dict = {}
+    for symbol in all_symbols_list:
+        sorted_list_all_points_of_company = filtered_ascend_sign_points_dataframe[symbol].tolist()
+        top_points_of_company = [i for i in sorted_list_all_points_of_company if i >= sign_percent_filter]
+
+        if len(top_points_of_company) < minimum_eligible_companies:
+            continue
+
+        company_temp_dict = {}
+        company_temp_dict['mean_points'] = round(statistics.mean(top_points_of_company), 3)
+        company_temp_dict['total_companies_included'] = len(top_points_of_company)
+
+        top_points_dict[symbol] = company_temp_dict
+
+    j = 0
+    for i in top_points_dict:
+        print(i)
+        print(top_points_dict[i])
+        print("###############################")
+        j += 1
+        if j > 10:
+            break"""
