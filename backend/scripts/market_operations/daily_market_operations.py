94df43ab0facd7d3629cfb3d434b9c3212901fd7
ori.f <orifreedman@gmail.com>
2020-07-03 19:54:04 +0300

    added notes to help proceed tomorrow

--- backend/scripts/market_operations/daily_market_operations.py
+++ backend/scripts/market_operations/daily_market_operations.py
@@ -31,21 +31,23 @@ def get_filtered_selected_points_dataframe(exchange_name, ascend_or_descend, sig
 
 def company_points_giving_by_filter(requested_points_dataframe, filtered_points_dataframe,
                                     all_symbols_list, company_symbol, percent_filter=0.8):
+    # pd.at[] doesnt work here, so i had to take the dataframe apart and recreate it at the end.
     requested_points_dataframe_symbols = list(requested_points_dataframe)
     requested_points_dataframe_points = requested_points_dataframe.iloc[0].tolist()
     requested_points_dataframe_count = requested_points_dataframe.iloc[1].tolist()
 
     company_points_giving_serie = filtered_points_dataframe.loc[[company_symbol]]
     for symbol_runner in all_symbols_list:
+        # ALL THE DIFFERENCE BETWEEN THE CHANCE POINTS AND THE VALUE POINTS IS HERE!
+        # TO CALCULATE THE POINTS FOR THE VALUE CASE, TAKE THE POINTS VAR CREATED JUST BELOW
+        # AND MULTIPLY IT BY TODAY'S PERCENT-CHANGE VALUE OF 1 OF THE COMPANIES (decide tomorrow which of them)
+        # THEN ADD THAT TO THE 'requested_points_dataframe_points'
         points = company_points_giving_serie.at[company_symbol, symbol_runner]
         if points >= percent_filter:
             index = requested_points_dataframe_symbols.index(symbol_runner)
 
             requested_points_dataframe_points[index] += points
             requested_points_dataframe_count[index] += 1
-
-            """requested_points_dataframe.at[0, symbol_runner] += points
-            requested_points_dataframe.at[1, symbol_runner] += 1"""
 
     updated_requested_points_dict = \
         {requested_points_dataframe_symbols[i]: [requested_points_dataframe_points[i],
@@ -60,14 +62,11 @@ def company_points_giving_by_filter(requested_points_dataframe, filtered_points_
 
 
 def assign_today_points(exchange_dataframe_today, exchange_name, delay_days,
-                        col_name, ascend_or_descend, sign_or_value, percent_filter=0.8, value_symbols=0):
+                        col_name, ascend_or_descend, sign_or_value, percent_filter=0.8):
     filtered_points_dataframe = \
         get_filtered_selected_points_dataframe(exchange_name, ascend_or_descend, sign_or_value, delay_days)
 
-    if (value_symbols != 0) and (sign_or_value == 'value'):
-        all_symbols_list = value_symbols
-    else:
-        all_symbols_list = list(filtered_points_dataframe)
+    all_symbols_list = list(filtered_points_dataframe)
 
     init_dict_for_dataframe = {symbol: [0, 0] for symbol in all_symbols_list}
     requested_points_dataframe = pd.DataFrame(data=init_dict_for_dataframe)
@@ -104,24 +103,32 @@ def assign_today_points(exchange_dataframe_today, exchange_name, delay_days,
             continue
 
     requested_points_dataframe = requested_points_dataframe.rename(index={0: 'points', 1: 'companies_count'}).T
-    #requested_points_dataframe = requested_points_dataframe.T
     return requested_points_dataframe
 
 
-
-def top_stocks_today(exchange_name, delay_days, sign_percent_filter=0.8, minimum_eligible_companies=3):
+def top_stocks_today(exchange_name, delay_days, sign_percent_filter=0.8, top_companies_number=10):
     exchange_dataframe_today = stocks_API.AllDataAnalysisToday(exchange_name).all_daily_dataframe
     exchange_dataframe_today_filtered = remove_companies_black_list_from_dataframe(exchange_dataframe_today)
     exchange_dataframe_today_filtered = exchange_dataframe_today_filtered.set_index('Symbol')
 
     today_points_ascend_sign_dataframe = assign_today_points(exchange_dataframe_today_filtered, exchange_name, delay_days,
-                                                 'Percent-Change', 'ascend', 'sign', sign_percent_filter, 0)
+                                                 'Percent-Change', 'ascend', 'sign', sign_percent_filter)
 
+    # that dataframe should be saved in its own xlsx file:
     today_points_ascend_sign_dataframe.sort_values(by=['points'], ascending=False, inplace=True)
 
+    # thats for later use:
+    top_chance_companies_symbols = today_points_ascend_sign_dataframe.head(top_companies_number).index
 
-    # sort the today_points_ascend_sign_dataframe to get the top chances for today,
-    # then get their symbols, then call assign_today_points again with the last argument as those symbols list.
 
 
+    today_points_ascend_value_dataframe = \
+        assign_today_points(exchange_dataframe_today_filtered, exchange_name, delay_days,
+                            'Percent-Change', 'ascend', 'value', sign_percent_filter)
+
+    print(today_points_ascend_value_dataframe)
+
+
+
+# ['VTIQW', 'VTIQ', 'NIU', 'FMB', 'NEBU', 'VTIQU', 'TUES', 'CSFL', 'BCLI', 'VRIG']
 
